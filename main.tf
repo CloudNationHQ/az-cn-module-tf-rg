138cff3d5ee46e72de67ad741f2fad87c90e05b8
dkool <dennis.kool@cloudnation.nl>
2023-11-03 11:43:12 +0100

    feat: add management lock support at resource group level

--- main.tf
+++ main.tf
@@ -5,3 +5,14 @@ resource "azurerm_resource_group" "groups" {
   name     = each.value.name
   location = each.value.region
 }
+
+# locks
+resource "azurerm_management_lock" "lock" {
+  for_each = {
+    for k, v in var.groups : k => v if v.management_lock != null
+  }
+  name       = "lock-${each.key}"
+  scope      = azurerm_resource_group.groups[each.key].id
+  lock_level = each.value.management_lock.level
+  notes      = try(each.value.management_lock.notes, null)
+}
