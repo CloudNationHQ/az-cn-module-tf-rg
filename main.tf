008f8c04c06dfb2b4fbb8f5be74b5d3091d07c16
dkool <dennis.kool@cloudnation.nl>
2023-11-03 14:59:48 +0100

    fix: management lock should be fully optional

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ resource "azurerm_resource_group" "groups" {
 # locks
 resource "azurerm_management_lock" "lock" {
   for_each = {
-    for k, v in var.groups : k => v if v.management_lock != null
+    for k, v in var.groups : k => v if try(v.management_lock != null, false)
   }
   name       = "lock-${each.key}"
   scope      = azurerm_resource_group.groups[each.key].id
-  lock_level = each.value.management_lock.level
+  lock_level = try(each.value.management_lock.level, "CanNotDelete")
   notes      = try(each.value.management_lock.notes, null)
 }
